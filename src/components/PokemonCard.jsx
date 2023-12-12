fd1d023cddc7588544bbc602c1473a91c9c451e5
ERIC BESSON <e-besson@laposte.net>
2023-12-12 13:52:03 +0100

    React Basics 03

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,29 @@
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+
+
 function PokemonCard() {
-    return <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />;
-    <figcaption> Bulbasaur</figcaption>
+
+    const pokemon = pokemonList[1];
+    
+    return (
+     <figure >
+        {pokemon.imgSrc != null ? (<img src={pokemon.imgSrc} alt={pokemon.name}/>): (<p>"???"</p>) }
+        <figcaption> {pokemon.name} </figcaption>
     </figure>
-   
-  
+    ); 
 }
 
+
 export default PokemonCard;
+
+
