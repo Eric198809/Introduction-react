7fa45eb559c7b06173677a9aafb8b08e20f64daf
ERIC BESSON <e-besson@laposte.net>
2023-12-12 18:21:17 +0100

    React Basics 04 ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import PokemonCard from './components/PokemonCard'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const pokemonList = [
+    {
+      name: "BULBASAUR",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "MEW",
+    },
+  ];
+  
   return (
-    <>
-      <div>
-       <PokemonCard/>
-      </div>
-      
-    </>
-  )
+    <PokemonCard pokemon = {pokemonList[0]}/>  
+  );
 }
 
 export default App
+
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,29 +1,13 @@
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
-
-
-
-function PokemonCard() {
-
-    const pokemon = pokemonList[1];
-    
+function PokemonCard({pokemon}) {
+  console.log(pokemon);
     return (
-     <figure >
-        {pokemon.imgSrc != null ? (<img src={pokemon.imgSrc} alt={pokemon.name}/>): (<p>"???"</p>) }
-        <figcaption> {pokemon.name} </figcaption>
-    </figure>
-    ); 
+    <>
+      <figure>
+        <img src = {pokemon.imgSrc} alt = {pokemon.name + " card"} />
+        <figcaption>{pokemon.name}</figcaption>
+        </figure>     
+    </>
+    )
 }
 
-
 export default PokemonCard;
-
-
